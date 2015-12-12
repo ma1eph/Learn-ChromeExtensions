cc36bf53afe731a603b7528884b518a8ab0e0d6c
ushizaa <lounge.act.2011@gmail.com>
2015-12-12 13:40:36 +0900

    Fix ReadTextFile

--- ReadTextFile/background.js
+++ ReadTextFile/background.js
@@ -1,13 +1,21 @@
-var file = 'data.txt';
-chrome.browserAction.onClicked.addListener(function(tab){
+var config = {};
+
+{
+  var file = 'config.json';
   var xhr = new XMLHttpRequest();
   xhr.open('GET', chrome.extension.getURL(file), true);
-  xhr.onreadystatechange = function()
-  {
-    if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200)
-    {
-      window.alert(xhr.responseText);
+  xhr.onreadystatechange = function() {
+    if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+      config = JSON.parse(xhr.responseText);
     }
   };
   xhr.send();
+}
+
+chrome.browserAction.onClicked.addListener(function(tab){
+  for (key in config) {
+    if (typeof config[key] === 'string' && config[key] != '') {
+      console.log(key + ': ' + config[key]);
+    }
+  }
 });
