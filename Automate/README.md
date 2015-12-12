fd1d471ef59ca1415f7b00347906108060710138
ushizaa <lounge.act.2011@gmail.com>
2015-12-12 13:44:33 +0900

    Remove Automate

--- Automate/README.md
+++
@@ -1,7 +0,0 @@
-# Automate
-
-## Required
-
-### json2.js
-
-- [https://github.com/douglascrockford/JSON-js](https://github.com/douglascrockford/JSON-js)
--- Automate/myscript.js
+++
@@ -1,5 +0,0 @@
-
-document.getElementById('a2').value='orange';
-document.getElementById('a3').value='yellow';
-document.getElementById('a4').value='green';
-document.getElementById('a5').value='blue';
